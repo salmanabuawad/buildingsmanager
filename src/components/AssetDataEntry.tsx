3f2936b2871415ec05ff9a575a4a1b6e00ecdb51
salmanabuawad <92875770+salmanabuawad@users.noreply.github.com>
2025-11-16 22:58:48 +0200

    Updated AssetDataEntry.tsx

--- src/components/AssetDataEntry.tsx
+++ src/components/AssetDataEntry.tsx
@@ -1192,7 +1192,7 @@ export function AssetDataEntry() {
     {
       field: 'sub_asset_size_6',
       headerName: t('subAssetSize6'),
-
+      minWidth: 200,
       editable: true,
       type: 'numericColumn',
       valueFormatter: (params) => params.value ? params.value.toFixed(2) : '',

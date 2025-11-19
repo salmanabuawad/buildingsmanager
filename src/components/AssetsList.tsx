a86297b7386727b1ee7eb0886cc5543823702d37
salmanabuawad <92875770+salmanabuawad@users.noreply.github.com>
2025-11-19 20:51:54 +0200

    Updated AssetsList.tsx

--- src/components/AssetsList.tsx
+++ src/components/AssetsList.tsx
@@ -223,6 +223,12 @@ export function AssetsList({ buildingNumber, taxZone, onSelectAsset }: AssetsLis
         assetValidators.validateAssetType(updatedAsset.sub_asset_type_4, 'sub_asset_type_4'),
         assetValidators.validateAssetType(updatedAsset.sub_asset_type_5, 'sub_asset_type_5'),
         assetValidators.validateAssetType(updatedAsset.sub_asset_type_6, 'sub_asset_type_6'),
+        assetValidators.validateSubAssetTypeComplete(updatedAsset.building_number, updatedAsset.sub_asset_type_1, updatedAsset.sub_asset_size_1),
+        assetValidators.validateSubAssetTypeComplete(updatedAsset.building_number, updatedAsset.sub_asset_type_2, updatedAsset.sub_asset_size_2),
+        assetValidators.validateSubAssetTypeComplete(updatedAsset.building_number, updatedAsset.sub_asset_type_3, updatedAsset.sub_asset_size_3),
+        assetValidators.validateSubAssetTypeComplete(updatedAsset.building_number, updatedAsset.sub_asset_type_4, updatedAsset.sub_asset_size_4),
+        assetValidators.validateSubAssetTypeComplete(updatedAsset.building_number, updatedAsset.sub_asset_type_5, updatedAsset.sub_asset_size_5),
+        assetValidators.validateSubAssetTypeComplete(updatedAsset.building_number, updatedAsset.sub_asset_type_6, updatedAsset.sub_asset_size_6),
       );
 
       // Run all validations
